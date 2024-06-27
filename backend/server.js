d9a12611f9b5b851f240a2d1603e660efda39b06
manishgupta2942 <manishgupta_mc21a11_75@dtu.ac.in>
2024-06-27 20:44:20 +0530

    Your commit message describing the updates

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
+
+
+// middlewares
+app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization','token'],
+  optionsSuccessStatus: 204, // For legacy browsers (IE11, various SmartTVs) that choke on 204
+  credentials: true, // Include this if your client needs to send credentials with requests
+}));
+// app.use(cors())
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
